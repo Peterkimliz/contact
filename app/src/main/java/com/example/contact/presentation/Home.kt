bd4799ac7c4e0c102adfc4f151bb746fb4b4dcf8
PeterLojiKim <pkironji@yahoo.com>
2024-03-01 14:39:44 +0300

    added the add contact page

--- app/src/main/java/com/example/contact/presentation/Home.kt
+++ app/src/main/java/com/example/contact/presentation/Home.kt
@@ -14,13 +14,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.example.contact.presentation.common.AppBar
+import com.example.contact.presentation.navigation.Navigations
 
 @Composable
+
 fun Home(navHostController: NavHostController) {
     Scaffold (
         topBar = {
-            AppBar(title = "Contacts")
+            AppBar(title = "Contacts",navHostController=navHostController)
         },
         floatingActionButton = {
             FloatingActionButton(
@@ -28,6 +31,7 @@ fun Home(navHostController: NavHostController) {
                 containerColor = MaterialTheme.colorScheme.primary,
                 contentColor = MaterialTheme.colorScheme.onPrimary,
                 onClick = {
+                    navHostController.navigate(route = Navigations.AddContact.route)
 
             }) {
                 Icon(imageVector = Icons.Default.Add, contentDescription ="Add contact" )
@@ -46,4 +50,10 @@ fun Home(navHostController: NavHostController) {
     }
 
 
+}
+
+@Preview
+@Composable
+fun PreviewHome(){
+ Home(navHostController = rememberNavController())
 }
--- app/src/main/java/com/example/contact/presentation/common/AppBar.kt
+++ app/src/main/java/com/example/contact/presentation/common/AppBar.kt
@@ -1,22 +1,31 @@
 package com.example.contact.presentation.common
+import android.annotation.SuppressLint
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AppBar(title:String,showLeading:Boolean=false) {
+fun AppBar(title:String,showLeading:Boolean=false,navHostController: NavHostController) {
 
     TopAppBar(
         navigationIcon = {
             if(showLeading)
-            Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "back")
+                IconButton(onClick = {
+                 navHostController.popBackStack()
+                }) {
+                    Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "back")
+                }
+
         },
         title = {
                 Text(text =title)
